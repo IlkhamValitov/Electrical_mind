2a3efb03472e1c7704ad85bb66d3562a39e26765
Creator <devmailmail4@gmail.com>
2023-06-11 15:13:04 +0400

    Создана базовая структура проекта

---
+++ index.php
@@ -0,0 +1,47 @@
+<html lang="ru">
+    <head>  
+        <meta charset="utf-8"/>
+        <title>Electrical mind</title>
+        <link rel="stylesheet" href="style/style.css">
+        <script src="script/script.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div id="outer_container">
+            <div class="main_container">
+                <header class="header_block">
+                    <div class="logo">
+                        <h2 class="logo_title">Electrical Mind</h2>
+                    </div>
+                    
+                    <nav class="navigate_block">
+                        <a href="#" class="button">Галерея</a>
+                        <a href="#" class="button">Экскурс в инсторию</a>
+                        <a href="#" class="button">О чем речь?</a>
+                        <a href="#" class="button">Контакты</a>
+                    </nav>
+
+                </header>
+                <div class="center_block">
+                    <section class="center_section">
+                        <div class="main_topic_block">
+                            <h1 class="main_topic">Искусственный интеллект: из вымысла в светлое будущее</h1>
+                            <!-- <img src="img/1609.jpg"/> -->
+                        </div>
+                        
+                        
+                        <h3 class="news_title">Новости<h3>
+                            
+
+                    </section>
+
+                </div>
+                <footer class="footer_block">
+                    <p class="author_inf">By IV</p>
+
+                </footer>
+
+            </div>
+        </div>
+    </body>
+</html>
