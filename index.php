9417899a77fb47df33fa02e538b9c2b53abd538e
Creator <devmailmail4@gmail.com>
2023-06-12 22:30:24 +0400

    Добавлен новый шрифт, а также "резиновые" настройки для блоков

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="style/style.css">
         <script src="script/script.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
     </head>
     <body> 
         <div id="outer_container">
